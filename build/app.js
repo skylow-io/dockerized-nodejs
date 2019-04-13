10b15423a62b5fbf87eb5d8248a5b559f090ffe7
Guillaume Ah-ki <guillaumeahki@gmail.com>
2019-04-12 21:49:11 -0400

    initial commit

---
+++ build/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// lib/app.ts
+const express_1 = __importDefault(require("express"));
+// Create a new express application instance
+const app = express_1.default();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
